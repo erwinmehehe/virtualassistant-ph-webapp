b7494de7f25be1b430afd2ec4f957ad10ac283f5
erwinmehehe <erwinvalles20@gmail.com>
2026-09-12 17:11:02 +0800

    Add public profile consent constants

---
+++ src/lib/privacy-consent.ts
@@ -0,0 +1,5 @@
+export const PUBLIC_PROFILE_CONSENT_VERSION = "2026-09-12-v1";
+export const PUBLIC_PROFILE_CONSENT_NOTICE_PATH = "/privacy";
+
+export const PUBLIC_PROFILE_CONSENT_SUMMARY =
+  "I agree that VirtualAssistant.com.ph may display my professional VA profile publicly so potential clients can evaluate me for work opportunities.";

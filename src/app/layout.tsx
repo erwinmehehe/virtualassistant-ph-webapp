08b4e88e4e04e4de845945dd3843ca9f057a8105
erwinmehehe <erwinvalles20@gmail.com>
2026-09-13 08:47:10 +0800

    Load public marketing design system

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import "./marketing-refresh.css";
 import "./talent-card-refresh.css";
 import "./premium-marketing-final.css";
+import "./va-design.css";
 
 const googleSiteVerification = process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION;
 const gaId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
